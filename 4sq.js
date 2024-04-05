90dae940995cca00ce3f4c33f9d2fd77fa2b9e31
asterein <88514774+asterein@users.noreply.github.com>
2024-04-05 18:03:22 -0400

    move all methods to class

--- 4sq.js
+++ 4sq.js
@@ -5,20 +5,6 @@ window.onload = () => {
         targetSq: null,
     }
 
-    let gameState = [
-        [null, null, null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-    ];
-
-    function randomGame() {
-    }
-
-    function testGame() {
-        
-    }
-
     function resetState() {
         if (state.userInputProgress) {
             state.userInputProgress = false;
@@ -49,10 +35,11 @@ window.onload = () => {
 }
 
 class Game {
-    constructor(dimension=2, inputs=[1,2,3,4]) {
+    constructor(dimension=2, inputs=[1,2,3,4], debug=false) {
         this.d = dimension;
         this.dimension = dimension;
         this.inputs = inputs;
+        this.debug = debug;
         this.board = this.generateGame(this.d, this.inputs);
     }
 
@@ -84,101 +71,133 @@ class Game {
         if (autoPrint) console.log(board);
         return board;
     }
+
+    validate() {
+        if (!this.testGameIsSquare()) return [false, 'sq'];
+        if (!this.testRows(this.debug)) return [false, 'r0'];
+        if (!this.testColumns(this.debug)) return [false, 'c0'];
+        if (!this.testBlocks(this.debug)) return [false, 'bl'];
+        return [true, null];
+    }
+
+    validateSection(section=[1,2,3,4]) {
+        if (section.length != this.inputs.length) return false;
+        if (section == null || this.inputs == null) return false;
+        const sortedSection = section.sort();
+        const sortedInputs = this.inputs.sort();
+        for (let i = 0; i < this.inputs.length; i++) {
+            if (sortedSection[i] != sortedInputs[i]) return false;
+        }
+        return true;
+    }
+
+    testGameIsSquare() {
+        // make sure the game is square
+        const gameHeight = this.board.length; // get height by counting rows
+        for (let i = 0; i < this.board.length; i++) {
+            // each row length should match height
+            if (this.board[i].length != gameHeight) return false;
+        }
+        return true;
+    }
+
+    testRows(debug=false) {
+        if (debug) console.log("ROWS: %o", this.board);
+        // validate each row
+        for (let i = 0; i < this.board.length; i++) {
+            if(!this.validateSection(this.board[i])) return false;
+        }
+        return true;
+    }
+
+    testColumns(debug=false) {
+        // height and width should be the same
+        const columnHeight = this.board.length;
+        // generate columns
+        const columns = [];
+        for (let c = 0; c < columnHeight; c++) {
+            let column = [];
+            for (let r = 0; r < columnHeight; r++) {
+                column.push(this.board[r][c]);
+            }
+            columns.push(column);
+        }
+        if (debug) console.log("COLS: %o", columns);
+        // make sure the game is still square
+        if (columns.length != this.board.length) return false;
+        // validate each column
+        for (let i = 0; i < columns.length; i++) {
+            if (!this.validateSection(columns[i])) return false;
+        }
+        return true;
+    }
+
+    testBlocks(debug=false) {
+        const blockCount = this.dimension ** 2;
+        const gameDimension = this.board.length;
+        // game dimension must be multiple of dimension
+        if (gameDimension % this.dimension !== 0) return false;
+        // split rows
+        const innerBlockCount = gameDimension/this.dimension;
+        const splitRows = [];
+        for (let y = 0; y < gameDimension; y++) {
+            let splitRow = [];
+            let row = this.board[y];
+            for (let i = 0; i < innerBlockCount; i++) {
+                splitRow.push([]);
+            }
+            let innerBlockIndex = 0;
+            for (let x = 0; x < gameDimension; x++) {
+                splitRow[innerBlockIndex].push(row[x]);
+                if (x+1 === this.dimension + (innerBlockIndex * this.dimension)) {
+                    innerBlockIndex += 1;
+                }
+            }
+            splitRows.push(splitRow);
+        }
+        // generate blocks
+        const blocks = [];
+        const origin = {
+            x: 0,
+            y: 0,
+        }
+        let count = 0;
+        while (count < blockCount) {
+            let y = origin.y;
+            let x = origin.x;
+            let block = splitRows[y][x].toString();
+            for (let i = 1; i < this.dimension; i++) {
+                block += "," + splitRows[y + i][x].toString();
+            }
+            blocks.push(block.split(","));
+            if (x +1 === this.dimension) {
+                origin.x = 0;
+                origin.y += this.dimension;
+            } else if (x < this.dimension) {
+                origin.x += 1;
+            }
+            count += 1;
+        }
+        if (blocks.length !== blockCount) return false;
+        if (debug) console.log(blocks);
+        // validate each block
+        for (let i = 0; i < blocks.length; i++) {
+            if(!this.validateSection(blocks[i])) return false;
+        }
+        return true;
+    }
 }
 
 function test() {
     const gameState = new Game(2, [1,2,3,4]);
     gameState.print();
-    testBlocks(gameState.board, 2);
+    console.log(gameState.validate());
 
     const game9 = new Game(3, [1,2,3,4,5,6,7,8,9]);
+    game9.debug = true;
     game9.print();
-    testBlocks(game9.board, 3);
+    console.log(game9.validate());
 }
 
-function validateGame(game, dimension=2) {
-    // check a game is valid
-    // returns [ bool, errorCode: string ]
-    if (!testGameIsSquare(game)) return [false, 'sq'];
-    if (!testRows(game)) return [false, 'r0'];
-    if (!testColumns(game)) return [false, 'c0'];
-    if (!testBlocks(game, dimension)) return [false, 'bl'];
-    return [true, null];
-}
 
-function validateSection(section=['1', '2', '3', '4'], expected=['1', '2', '3', '4']) {
-    if (section.length != expected.length) return false;
-    if (section == null || expected == null) return false;
-    const sorted = section.sort();
-    for (let i = 0; i < expected.length; i++) {
-        if (sorted[i] != expected[i]) return false;
-    }
-    return true;
-}
-
-function testBlocks(game, dimension=2) {
-    const blockCount = dimension ** 2;
-    const gameDimension = game.length;
-    // game dimension must be multiple of dimension
-    if (gameDimension % dimension !== 0) return false;
-    // generate blocks
-    const innerBlockCount = gameDimension/dimension;
-    const splitRows = [];
-    for (let y = 0; y < gameDimension; y++) {
-        let splitRow = [];
-        let row = game[y];
-        for (let i = 0; i < innerBlockCount; i++) {
-            splitRow.push([]);
-        }
-        let innerBlockIndex = 0;
-        for (let x = 0; x < gameDimension; x++) {
-            splitRow[innerBlockIndex].push(row[x]);
-            if (x+1 === dimension + (innerBlockIndex * dimension)) {
-                innerBlockIndex += 1;
-            }
-        }
-        splitRows.push(splitRow);
-    }
-    console.log(splitRows);
-    return true;
-}
-
-function testGameIsSquare(game) {
-    // make sure the game is square
-    const gameHeight = game[0].length; // get height by counting rows
-    for (let i = 0; i < game.length; i++) {
-        // each row length should match height
-        if (game[i].length != gameHeight) return false;
-    }
-    return true;
-}
-
-function testColumns(game, debug=false) {
-    // generate columns
-    const columns = [];
-    for (let c = 0; c < columnHeight; c++) {
-        let column = [];
-        for (let r = 0; r < columnHeight; r++) {
-            column.push(game[r][c]);
-        }
-        columns.push(column);
-    }
-    if(debug) console.log("COLS: %o", columns);
-    // make sure the game is still square
-    if (columns.length != game.length) return false;
-    // validate each column
-    for (let i = 0; i < columns.length; i++) {
-        if (!validateSection(columns[i])) return false;
-    }
-    return true;
-}
-
-function testRows(game, debug=false) {
-    if(debug) console.log("ROWS: %o", game);
-    // validate each row
-    for (let i = 0; i < game.length; i++) {
-        if(!validateSection(game[i])) return false;
-    }
-    return true;
-}
 
