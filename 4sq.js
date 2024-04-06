64a26d9705f13c61c2dea4ba2fbb27977aef7186
asterein <88514774+asterein@users.noreply.github.com>
2024-04-05 20:59:23 -0400

    keep original type

--- 4sq.js
+++ 4sq.js
@@ -47,11 +47,15 @@ class Game {
         this.#board = this.#generate(threshold);
     }
 
-    print(autoPrint=true) {
+    print(game=this.#board, autoPrint=true) {
+        if (game === null) return;
+        if (game === "_") {
+            game = this.#board;
+        }
         let board = "";
-        for (let y = 0; y < this.#board.length; y++) {
+        for (let y = 0; y < game.length; y++) {
             let printRow = "";
-            let row = this.#board[y];
+            let row = game[y];
             for (let x = 0; x < row.length; x++) {
                 printRow += ` ${row[x]}`;
             }
@@ -61,11 +65,12 @@ class Game {
         return board;
     }
 
-    validate() {
+    validate(game=this.#board) {
+        if (game === null) return;
         if (!this.#testGameIsSquare()) return false;
-        const rows = this.getRows();
-        const cols = this.getColumns();
-        const blocks = this.getBlocks();
+        const rows = this.getRows(game);
+        const cols = this.getColumns(game);
+        const blocks = this.getBlocks(game);
         if (this.debug) {
             console.log("ROWS: %o", rows);
             console.log("COLS: %o", cols);
@@ -100,25 +105,25 @@ class Game {
         return this.#board;
     }
 
-    getRows() {
-        return this.#board;
+    getRows(game=this.#board) {
+        return game;
     }
 
-    getColumns() {
+    getColumns(game=this.#board) {
         const columns = [];
         for (let x = 0; x < this.#board.length; x++) {
             let col = [];
             for (let y = 0; y < this.#board.length; y++) {
-                col.push(this.#board[y][x]);
+                col.push(game[y][x]);
             }
             columns.push(col);
         }
         return columns;
     }
 
-    getBlocks() {
+    getBlocks(game=this.#board) {
         const blockCount = this.#dimension ** 2;
-        const gameDimension = this.#board.length;
+        const gameDimension = game.length;
         // game dimension must be multiple of dimension
         if (gameDimension % this.#dimension !== 0) return false;
         // split rows
@@ -126,7 +131,7 @@ class Game {
         const splitRows = [];
         for (let y = 0; y < gameDimension; y++) {
             let splitRow = [];
-            let row = this.#board[y];
+            let row = game[y];
             for (let i = 0; i < innerBlockCount; i++) {
                 splitRow.push([]);
             }
@@ -139,6 +144,7 @@ class Game {
             }
             splitRows.push(splitRow);
         }
+        if (this.debug) console.log("SPLIT ROWS: %o", splitRows);
         // generate blocks
         const blocks = [];
         const origin = {
@@ -149,11 +155,11 @@ class Game {
         while (count < blockCount) {
             let y = origin.y;
             let x = origin.x;
-            let block = splitRows[y][x].toString();
+            let block = splitRows[y][x];
             for (let i = 1; i < this.#dimension; i++) {
-                block += "," + splitRows[y + i][x].toString();
+                block = [].concat(block, splitRows[y+i][x]);
             }
-            blocks.push(block.split(","));
+            blocks.push(block);
             if (x +1 === this.#dimension) {
                 origin.x = 0;
                 origin.y += this.#dimension;
@@ -176,14 +182,24 @@ class Game {
             if (this.debug) console.log("Successfully generated a board after " + count + " attempts");
             return tmp;
         }
-        console.error("Failed to generate a board after " + max + " attempts");
+        console.error("Failed to generate a board after " + threshold + " attempts");
         return null;
+    }
+
+    #blankBoard() {
+        const blank = [];
+        for (let y = 0; y < this.#dimension ** 2; y++) {
+            let row = new Array(this.#dimension).fill(null);
+            blank.push(row);
+        }
+        return blank;
     }
 
     #newGame(dimension, inputs) {
         if (dimension <= 0) return null;
         if (inputs.length != dimension ** 2) return null;
         const game = [];
+        const fill = this.#blankBoard();
         for (let y = 0; y < dimension ** 2; y++) {
             let usableRowInputs = inputs;
             let row = [];
@@ -203,6 +219,14 @@ class Game {
                 }
                 let input = usableInputs[randIndex];
                 row.push(input);
+                fill[y][x] = input;
+                /*
+                let tmpBlocks = this.getBlocks(fill);
+                if (this.debug) console.log(tmpBlocks);
+                for (let i = 0; i < this.#dimension ** 2; i++) {
+                    if (!this.#validateSection([...tmpBlocks[i]], false)) return null;
+                }
+                */
                 usableRowInputs = [...usableRowInputs].filter(val => val != input);
             }
             game.push(row);
@@ -210,13 +234,16 @@ class Game {
         return game;
     }
 
-    #validateSection(section=[1,2,3,4]) {
+    #validateSection(section=[1,2,3,4], strict=true) {
         if (section.length != this.#inputs.length) return false;
         if (section == null || this.#inputs == null) return false;
         const sortedSection = section.sort();
         const sortedInputs = this.#inputs.sort();
         for (let i = 0; i < this.#inputs.length; i++) {
-            if (sortedSection[i] != sortedInputs[i]) return false;
+            if (sortedSection[i] != sortedInputs[i]) {
+                if (strict) return false;
+                if (sortedSection[i] !== null) return false;
+            }
         }
         return true;
     }
